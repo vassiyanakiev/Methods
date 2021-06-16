9fcf9027567b2ff8dce779a5ee51d6cf16ddd563
Vasilena Georgieva <75674763+vassiyanakiev@users.noreply.github.com>
2021-06-16 15:49:27 +0300

    methods

---
+++ MethodExercises/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MethodExercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+
+            int result = GetSmallestOfTheNumbers(firstNumber, secondNumber, thirdNumber);
+            Console.WriteLine(result);
+        }
+
+        private static int GetSmallestOfTheNumbers(int firstNumber, int secondNumber, int thirdNumber)
+        {
+            
+            if(firstNumber < secondNumber && firstNumber < thirdNumber)
+            {
+                return firstNumber;
+            }
+            else if(secondNumber < firstNumber && secondNumber < thirdNumber)
+            {
+                return secondNumber;
+            }
+            else
+            {
+                return thirdNumber;
+            }
+        }
+
+    }
+}
